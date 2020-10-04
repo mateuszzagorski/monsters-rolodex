5763c2028b108f8ff9e43c292e41221aeb2dfda1
Mateusz Zagórski <mzagorski@squiz.pl>
2020-10-04 18:59:50 +0200

    Updates

---
+++ precache-manifest.a1738cf8cd9818747c18841b072b20ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bdbb6c0667b9911c0b6c7d96e48280c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "69b930f93b605ba3be40",
+    "url": "/monsters-rolodex/static/css/main.cc0465d3.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69b930f93b605ba3be40",
+    "url": "/monsters-rolodex/static/js/main.52ac07e5.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
